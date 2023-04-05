b698b1b785b2c0c47ba1018bc5f08d1a006dcb73
Ryan Kim <rj.kim.toronto@gmail.com>
2023-04-05 00:10:22 -0400

    GPT api generic struture

--- chatgpt.py
+++ chatgpt.py
@@ -1,4 +1,13 @@
+import openai
+from API_SECRETS import API_KEY
 
-class GptInterface:
+openai.api_key = API_KEY
+
+class Model:
     def __init__(self):
-        pass
+        self.model = "text-davinci-003"
+        self.temperature = 0.6
+        self.max_tokens = 100
+
+    def run(self, prompt):
+        return openai.Completion.create(self.model, prompt, self.temperature, self.max_tokens)
