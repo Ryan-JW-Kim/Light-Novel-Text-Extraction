652e38276b52f6341a593588322288a328c1a116
Ryan Kim <rj.kim.toronto@gmail.com>
2023-05-15 23:08:05 -0400

    updates

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import threading
 from files import *
 from network import SiteSpecificScraper
 import sys
+import requests
 sys.path.insert(0, "../Python-Proxy")
 from proxy import ProxyParser, ProxyBatch
 from proxy_list_scraper import Scraper
@@ -44,6 +45,35 @@ def testing():
         File_Writer.write_text_file(result["Text"], directory, count)
         count += 1
 
+def testing_new():
+    
+
+    book_webdata = SiteSpecificScraper()    
+    directory = f"downloaded_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+    File_Writer.create_directory(directory)
+
+    scraper = Scraper()
+    scraper.save_proxies()
+
+    count = 1
+    i = 0
+    for proxy in ProxyParser.valid_proxy_generator("scraped_proxies.txt"):
+        
+        try:
+            proxy = {"http": f"http://{proxy['IP Address']}:{proxy['Port']}"}
+            print(proxy)
+            result = requests.get(book_webdata.urls[i], proxies=proxy)
+            print(result.text)
+            File_Writer.write_text_file(result, directory, count)
+            
+            i += 1
+            count += 1
+    
+        except:
+            print(f"Error with proxy: {proxy}")
+            continue
+
+
 if __name__ == '__main__':
     
-    testing()
+    testing_new()
